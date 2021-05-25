e396e6d992d2a8f419310b53b846299af1a4b5b0
UnknownBug <1256623408@qq.com>
2021-05-25 23:37:00 +0800

    命令行模式

---
+++ CommandPattern/main.cc
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <list>
+#include <memory>
+#include <algorithm>
+
+class Barbecuer
+{
+public:
+    void BakeMutton() {
+        std::cout << "BakeMutton" << std::endl;
+    }
+
+    void BakeChickenWing() {
+        std::cout << "BakeChickenWingCmd" << std::endl;
+    }
+};
+
+class ICommand
+{
+public:
+    ICommand(Barbecuer receiver)
+        : _receiver(receiver)
+    {
+    }
+    virtual ~ICommand() {}
+
+    std::string name() const
+    {
+        return _name;
+    }
+
+    virtual void excute() = 0;
+
+protected:
+    std::string _name;
+    Barbecuer _receiver;
+};
+
+class BakeMuttonCmd : public ICommand
+{
+public:
+    BakeMuttonCmd(Barbecuer receiver) : ICommand(receiver)
+    {
+        _name = std::string("BakeMutton");
+    }
+
+    void excute() override
+    {
+        _receiver.BakeMutton();
+    }
+};
+
+class BakeChickenWingCmd : public ICommand
+{
+public:
+    BakeChickenWingCmd(Barbecuer receiver) : ICommand(receiver)
+    {
+        _name = std::string("BakeChickenWing");
+    }
+
+    void excute() override
+    {
+        _receiver.BakeChickenWing();
+    }
+};
+
+class Waiter
+{
+public:
+    //下单
+    void setOrder(const std::shared_ptr<ICommand> &cmd)
+    {
+        //如果菜卖完了 tips:换个菜吧
+        //else
+        _cmds.emplace_back(cmd);
+    }
+
+    //取消订单
+    void cancelOrer(const std::shared_ptr<ICommand> &cmd)
+    {
+        auto result = std::find(_cmds.begin(), _cmds.end(), cmd);
+        if (result != _cmds.end())
+        {
+            std::cout << "取消了" << cmd->name() << std::endl;
+            _cmds.erase(result);
+        }
+    }
+
+    void notify()
+    {
+        for (auto cmd : _cmds)
+        {
+            cmd->excute();
+        }
+    }
+
+private:
+    std::list<std::shared_ptr<ICommand>> _cmds;
+};
+
+int main()
+{
+    Barbecuer barbecuer;
+    Waiter waiter;
+
+    auto muttonCmd = std::make_shared<BakeMuttonCmd>(barbecuer);
+    auto chickenwingCmd = std::make_shared<BakeChickenWingCmd>(barbecuer);
+
+    waiter.setOrder(muttonCmd);
+    waiter.setOrder(chickenwingCmd);
+
+    waiter.notify();
+    
+    waiter.cancelOrer(muttonCmd);
+
+    waiter.notify();
+
+    return 0;
+}
